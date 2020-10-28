d8d5430c22333e12c5aab50165ed4ce7cf90b5a2
Sbuchelov <mikivolkoff@ya.ru>
2020-10-28 23:28:31 +0300

    added all method for task2 in lesson 3

--- src/lessons/Lesson3.java
+++ src/lessons/Lesson3.java
@@ -5,39 +5,82 @@ import java.util.Scanner;
 
 public class Lesson3 {
 
-    String word, patternWord;
+    String userWord, patternWord;
     String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
             "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea",
             "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
 
     public Lesson3() {
+        enterGame();
+        while (!userWord.equals(patternWord)) {
+            comparingWords();
+        }
+        System.out.println("Congratulate! You are win!");
+
+    }
+
+    void enterGame() {
         System.out.println("Welcome to the game!");
         System.out.println();
-        System.out.println("Enter the word:");
-        Scanner scanner = new Scanner(System.in);
-        this.setWord(scanner.next());
-        System.out.println("Your word: " + this.getWord());
         patternWord = getRandomFromWords();
-
-        comparingWords(word, patternWord);
-
-    }
-
-    private void comparingWords(String userWord, String pattWord) {
+        System.out.println("Enter your word:");
+        Scanner scanner = new Scanner(System.in);
+        this.setUserWord(scanner.next());
+        System.out.println("Your word: " + this.getUserWord());
 
     }
 
-    private String getRandomFromWords() {
+    void comparingWords() {
+        hideOtherCharCompMass(fillingCompMass(userWord, patternWord), calculateBaseLengthCharMass(userWord, patternWord));
+        System.out.println("Try again please:");
+        Scanner scanner = new Scanner(System.in);
+        this.setUserWord(scanner.next());
+    }
+
+    char[] fillingCompMass(String userWord, String patternWord) {
+        char[] compMass = new char[15];
+        char userChar, patternChar;
+        for (int i = 0; i < calculateBaseLengthCharMass(userWord, patternWord); i++) {
+            userChar = userWord.charAt(i);
+            patternChar = patternWord.charAt(i);
+            if (userChar == patternChar) {
+                compMass[i] = userChar;
+            } else {
+                compMass[i] = '#';
+            }
+        }
+        return compMass;
+    }
+
+    int calculateBaseLengthCharMass(String userWord, String patternWord) {
+        if (patternWord.length() >= userWord.length()) {
+            return userWord.length();
+        } else {
+            return patternWord.length();
+        }
+    }
+
+    void hideOtherCharCompMass(char[] compMass, int compMassLength) {
+        for (int i = compMassLength; i < 15; i++) {
+            compMass[i] = '#';
+        }
+        for (int i = 0; i < compMass.length; i++) {
+            System.out.print(compMass[i]);
+        }
+        System.out.println();
+    }
+
+    String getRandomFromWords() {
         int randomIndex = new Random().nextInt(words.length - 1);
         return words[randomIndex];
     }
 
-    public String getWord() {
-        return word;
+    public String getUserWord() {
+        return userWord;
     }
 
-    public void setWord(String word) {
-        this.word = word;
+    public void setUserWord(String userWord) {
+        this.userWord = userWord;
     }
 
 
