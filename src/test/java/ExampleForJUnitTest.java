b68f6257df4c1b822a99d0468b117f36bd80ee3d
Sbuchelov <mikivolkoff@ya.ru>
2021-01-25 21:19:59 +0300

    Level 3 lesson 6 task2,3 some syntax corrects

--- src/test/java/ExampleForJUnitTest.java
+++ src/test/java/ExampleForJUnitTest.java
@@ -28,30 +28,9 @@ public class ExampleForJUnitTest {
 
     public static Stream<Arguments> dataForAfterPast4massTest() {
         List<Arguments> out = new ArrayList<>();
-        int[] resultMass = new int[2], mass = new int[6];
-        int[] resultMass1 = new int[3], mass1 = new int[6];
-        int[] resultMass2 = new int[1], mass2 = new int[3];
-        mass[0] = 1;
-        mass[1] = 2;
-        mass[2] = 3;
-        mass[3] = 4;
-        mass[4] = 1;
-        mass[5] = 7;
-        resultMass[0] = mass[5];
-        resultMass[1] = mass[4];
-        mass1[0] = 1;
-        mass1[1] = 2;
-        mass1[2] = 4;
-        mass1[3] = 2;
-        mass1[4] = 1;
-        mass1[5] = 7;
-        resultMass1[0] = mass1[5];
-        resultMass1[1] = mass1[4];
-        resultMass1[2] = mass1[3];
-        mass2[0] = 1;
-        mass2[1] = 4;
-        mass2[2] = 7;
-        resultMass2[0] = 7;
+        int[] resultMass = new int[]{7, 1}, mass = new int[]{1, 2, 3, 4, 1, 7};
+        int[] resultMass1 = new int[]{7, 1, 2}, mass1 = new int[]{1, 2, 4, 2, 1, 7};
+        int[] resultMass2 = new int[]{7}, mass2 = new int[]{1, 4, 7};
 
         out.add(Arguments.arguments(resultMass, mass));
         out.add(Arguments.arguments(resultMass1, mass1));
@@ -61,10 +40,7 @@ public class ExampleForJUnitTest {
 
     @Test
     public void testAfterPast4massWithoutBorderElement() {
-        int[] mass = new int[3];
-        mass[0] = 1;
-        mass[1] = 2;
-        mass[2] = 3;
+        int[] mass = new int[]{1, 2, 3};
         Assertions.assertThrows(RuntimeException.class, () -> exampleForJUnit.afterPast4mass(mass));
     }
 
@@ -76,23 +52,9 @@ public class ExampleForJUnitTest {
 
     public static Stream<Arguments> dataHavingBorderElementTest() {
         List<Arguments> out = new ArrayList<>();
-        int[] mass = new int[6];
-        int[] mass1 = new int[4];
-        int[] mass2 = new int[4];
-        mass[0] = 1;
-        mass[1] = 2;
-        mass[2] = 3;
-        mass[3] = 4;
-        mass[4] = 1;
-        mass[5] = 7;
-        mass1[0] = 2;
-        mass1[0] = 2;
-        mass1[0] = 2;
-        mass1[0] = 2;
-        mass2[0] = 1;
-        mass2[0] = 2;
-        mass2[0] = 2;
-        mass2[0] = 2;
+        int[] mass = new int[]{1, 2, 3, 4, 1, 7};
+        int[] mass1 = new int[]{2, 2, 2, 2};
+        int[] mass2 = new int[]{1, 2, 2, 2};
         out.add(Arguments.arguments(true, mass));
         out.add(Arguments.arguments(false, mass1));
         out.add(Arguments.arguments(false, mass2));
